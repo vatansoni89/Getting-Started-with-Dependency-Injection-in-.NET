8a882f95925997a4eb8167e5fd5bd6b65619d81e
Vatan Soni <vatansoni89@gmail.com>
2019-11-22 00:40:01 +0530

    Add project files.

---
+++ PeopleViewer.View/PeopleViewerWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using PeopleViewer.Presentation;
+using System.Windows;
+
+namespace PeopleViewer.View
+{
+    public partial class PeopleViewerWindow : Window
+    {
+        private PeopleViewModel viewModel;
+
+        public PeopleViewerWindow(PeopleViewModel peopleViewModel)
+        {
+            InitializeComponent();
+            viewModel = peopleViewModel;
+            this.DataContext = viewModel;
+        }
+
+        private void FetchButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.RefreshPeople();
+            ShowRepositoryType();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.ClearPeople();
+            ClearRepositoryType();
+        }
+
+        private void ShowRepositoryType()
+        {
+            RepositoryTypeTextBlock.Text = viewModel.DataReaderType;
+        }
+
+        private void ClearRepositoryType()
+        {
+            RepositoryTypeTextBlock.Text = string.Empty;
+        }
+    }
+}
